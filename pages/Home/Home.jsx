b3d869f0404401818ca0c727b3b2a8fe77c4ae25
Naltpom <nathan.provost78990@gmail.com>
2023-09-30 16:18:23 +0200

    Coords de l'utilisateur

--- pages/Home/Home.jsx
+++ pages/Home/Home.jsx
@@ -1,12 +1,44 @@
 import { Text, View } from "react-native";
 import { s } from "./Home.style";
+import {
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+} from "expo-location";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [coords, setCoords] = useState([]);
+
+  useEffect(() => {
+    getUserLocation();
+  }, []);
+
+  async function getUserLocation() {
+    let { status } = await requestForegroundPermissionsAsync();
+    if (status === "granted") {
+      const location = await getCurrentPositionAsync();
+
+      setCoords({
+        lat: location.coords.latitude,
+        lng: location.coords.longitude,
+      });
+    } else {
+      setCoords({ lat: "48.85", lng: "2.35" });
+    }
+  }
+
+  console.log(coords);
   return (
     <>
-      <View style={s.meteo_basic}><Text>Hello</Text></View>
-      <View style={s.searchbar}><Text>Hello</Text></View>
-      <View style={s.meteo_advanced}><Text>Hello</Text></View>
+      <View style={s.meteo_basic}>
+        <Text>Hello</Text>
+      </View>
+      <View style={s.searchbar}>
+        <Text>Hello</Text>
+      </View>
+      <View style={s.meteo_advanced}>
+        <Text>Hello</Text>
+      </View>
     </>
   );
 }

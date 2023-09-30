30cf4d00b49ab8b989cd00b2d3092afb2e2b257e
Naltpom <nathan.provost78990@gmail.com>
2023-09-30 16:01:47 +0200

    Layout

--- pages/Home/Home.jsx
+++ pages/Home/Home.jsx
@@ -1,5 +1,12 @@
+import { Text, View } from "react-native";
 import { s } from "./Home.style";
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <View style={s.meteo_basic}><Text>Hello</Text></View>
+      <View style={s.searchbar}><Text>Hello</Text></View>
+      <View style={s.meteo_advanced}><Text>Hello</Text></View>
+    </>
+  );
 }

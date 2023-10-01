bea7b1510261c7069c1caf5293bf034f04789aff
Naltpom <nathan.provost78990@gmail.com>
2023-10-01 09:59:25 +0200

    Navigation

---
+++ pages/Forecast/Forecast.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import { s } from "./Forecast.style";
+import { Txt } from "../../compnents/Txt/Txt";
+
+export default function Forecast() {
+  return (
+    <>
+      <Txt>Hello</Txt>
+    </>
+  );
+}

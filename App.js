a947c82b29b3e7f967b60b5dcb1fbfecf3881b57
Naltpom <nathan.provost78990@gmail.com>
2023-10-01 14:56:46 +0200

    notification

--- App.js
+++ App.js
@@ -4,17 +4,65 @@ import AlataRegular from "./assets/fonts/Alata-Regular.ttf";
 import { useFonts } from "expo-font";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
+import Constants from "expo-constants";
 import Forecast from "./pages/Forecast/Forecast";
+import { Platform } from "react-native";
+import { useEffect } from "react";
+
 const Stack = createNativeStackNavigator();
 const navTheme = {
   colors: {
     background: "transparent",
   },
 };
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Device.isDevice) {
+    const { status: existingStatus } =
+      await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== "granted") {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== "granted") {
+      alert("Failed to get push token for push notification!");
+      return;
+    }
+    token = await Notifications.getExpoPushTokenAsync({
+      projectId: Constants.expoConfig.extra.eas.projectId,
+    });
+    console.log(token);
+  } else {
+    alert("Must use physical device for Push Notifications");
+  }
+
+  if (Platform.OS === "android") {
+    Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
+    });
+  }
+
+  return token;
+}
+
 export default function App() {
   const [isFontLoaded] = useFonts({
     "Alata-Regular": AlataRegular,
   });
+
+  useEffect(() => {
+    Notifications.addNotificationReceivedListener((res) => {
+      console.log("data", res.notification.request.content.data);
+    });
+    registerForPushNotificationsAsync();
+  }, []);
 
   return (
     <NavigationContainer theme={navTheme}>
